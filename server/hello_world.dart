27e9f6bb2b813ed350f7216ed32eed241701b93d
Göran Johansson <goran.johansson@longbay.se>
2014-04-27 12:12:36 +0200

    Update hello_world.dart

--- server/hello_world.dart
+++ server/hello_world.dart
@@ -5,7 +5,7 @@ class HelloWorld extends Vane {
   Future main() {
     if(query.containsKey("name")) {
       log.info("Saying hello to ${query["name"]}");
-      close("Greetings ${query["name"]}");
+      close("Hej ${query["name"]}");
     } else {
       log.info("Saying hello to the world");
       close("Hello World!");  
