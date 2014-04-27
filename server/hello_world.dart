910782cf29d4784813ee8209433d1c802e30914e
Göran Johansson <goran.johansson@longbay.se>
2014-04-27 13:39:57 +0200

    greeting changed

--- server/hello_world.dart
+++ server/hello_world.dart
@@ -5,12 +5,12 @@ class HelloWorld extends Vane {
   Future main() {
     if(query.containsKey("name")) {
       log.info("Saying hello to ${query["name"]}");
-      close("Hej ${query["name"]}");
+      close("Hello ${query["name"]}");
     } else {
       log.info("Saying hello to the world");
-      close("Hello World!");  
+      close("Hello World!");
     }
-    
+
     return end;
   }
 }
